3c86c84a74967875200fcd402225dcd3f11f8d66
dubass83 <makssych@gmail.com>
2017-01-04 17:53:43 +0200

    write setings to knife.rb

---
+++ .chef/knife.rb
@@ -0,0 +1,8 @@
+log_level                :info
+log_location             STDOUT
+node_name                'admin'
+client_key               '/home/maxim/chef-repo/.chef/admin.pem'
+validation_client_name   'maxcorp-validator'
+validation_key           '/home/maxim/chef-repo/.chef/maxcorp-validator.pem'
+chef_server_url          'https://chef-server.example.com/organizations/maxcorp'
+syntax_check_cache_path  '/home/maxim/chef-repo/.chef/syntax_check_cache'
