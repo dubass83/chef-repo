97d1107a192be43613a0ae62afd1ec189649245a
dubass83 <makssych@gmail.com>
2017-01-05 11:48:01 +0200

    tuning berks

--- cookbooks/my_cookbook/metadata.rb
+++ cookbooks/my_cookbook/metadata.rb
@@ -17,3 +17,7 @@ version '0.1.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/my_cookbook' if respond_to?(:source_url)
+depends 'build-essential'
+depends "chef-client"
+depends "apt"
+depends "ntp"
--- cookbooks/my_cookbook/recipes/default.rb
+++ cookbooks/my_cookbook/recipes/default.rb
@@ -3,3 +3,7 @@
 # Recipe:: default
 #
 # Copyright:: 2017, The Authors, All Rights Reserved.
+
+include_recipe "chef-client"
+include_recipe "apt"
+include_recipe "ntp"
